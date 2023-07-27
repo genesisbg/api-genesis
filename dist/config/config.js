8c8c765658ce75ca6fac79fc1b08018d94f85a93
SantiRendon <srendon34@soy.sena.edu.co>
2023-07-27 16:55:53 -0500

    sapa hp

--- dist/config/config.js
+++ dist/config/config.js
@@ -11,6 +11,7 @@ var _default = {
   host: process.env.DB_HOST || "",
   database: process.env.DB_NAME || "",
   user: process.env.DB_USER || "",
-  password: process.env.DB_PASSWORD || ""
+  password: process.env.DB_PASSWORD || "",
+  port: process.env.DB_PORT || ""
 };
 exports["default"] = _default;
--- dist/db/database.js
+++ dist/db/database.js
@@ -3,13 +3,22 @@
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 var _promiseMysql = _interopRequireDefault(require("promise-mysql"));
 var _config = _interopRequireDefault(require("../config/config"));
-//* se crea la conexion a la base de datos
-var connection = _promiseMysql["default"].createConnection({
+var connection = _promiseMysql["default"].createPool({
   host: _config["default"].host,
   database: _config["default"].database,
   user: _config["default"].user,
-  password: _config["default"].password
+  password: _config["default"].password,
+  port: _config["default"].port
 });
+
+//* se crea la conexion a la base de datos
+// const connection = mysql.createConnection({
+//     host: config.host,
+//     database: config.database,
+//     user: config.user,
+//     password: config.password,
+//     port: config.port,
+// });
 
 //* funcion que retorna la conexion
 var getConnection = function getConnection() {
